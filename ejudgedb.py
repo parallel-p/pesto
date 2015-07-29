cd355e7889daa61f0f0d0f3ffc716e3094303126
Artom-Kozincev <s17b2_kozincev@179.ru>
2015-07-29 15:07:25 +0300

    Resolves #14. Again.

--- ejudgedb.py
+++ ejudgedb.py
@@ -9,7 +9,7 @@ class EjudgeDB:
         self.data = {}
         with open(csv_filename) as csv_file:
             for line in csv_file:
-                ejudge_run_id, contest_id, user_id, problem_id, lang_id, status = map(lambda x: x[1:-1], line.split(';'))
+                ejudge_run_id, contest_id, user_id, problem_id, lang_id, status = line.replace('"', '').split(';')
                 if contest_ids is None or contest_id in contest_ids:
                     self.data[(contest_id, ejudge_run_id)] = EjudgeDBEntry(problem_id, user_id, lang_id)
     
