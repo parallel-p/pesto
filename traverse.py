04f4930c4150952bc70c3a933ed090ea0ad7f15c
Ivan Kriuchkov <johnhook059@gmail.com>
2015-07-29 15:57:16 +0500

    fixed traverse.py

--- traverse.py
+++ traverse.py
@@ -7,9 +7,9 @@ from gzip import open as gzip_open
 def traverse_contest(first_dir):
     for root, dirs, files in walk(first_dir):
         for f in files:
-            file_name = join(root, f)
-            if file_name[0] == '.':
+            if f[0] == '.':
                 continue
+            file_name = join(root, f)
             if file_name.endswith('.gz'):
                 current_file = gzip_open(file_name, encoding='utf-8')
             else:
