485c61910ec784b01fccaa15bd831dd44f97c1ba
Ivan Kriuchkov <johnhook059@gmail.com>
2015-07-29 14:42:39 +0500

    fixed traverse.py

--- traverse.py
+++ traverse.py
@@ -1,13 +1,13 @@
 __author__ = 'Kriuchkov'
 from os import walk
-from os import path
+from os.path import join
 import gzip
 
 
 def traverse_contest(my_dir):
-    for d, dirs, files in walk(my_dir):
+    for root, dirs, files in walk(my_dir):
         for f in files:
-            file_name = path.join(d, f)
+            file_name = join(root, f)
             if file_name.endswith('.gz'):
                 current_file = gzip.open(file_name, 'r')
             else:
