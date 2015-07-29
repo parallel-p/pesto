9110ae999f36d1548465bfdf94e6fb09ae199238
Dmitry Strokov <strokovghost@gmail.com>
2015-07-29 14:53:43 +0700

    Refs #12; Refs #15 Added comments, some cosmetic gizmos.

--- ejudge_get_submit.py
+++ ejudge_get_submit.py
@@ -1,21 +1,22 @@
 import xml.etree.ElementTree as ETree
-from run_class import Run
+from run import Run
 from problem import Problem
 from submit import Submit
 
 
 def ejudge_get_submit(file, memory_base, contest_id):
     lines = file.readlines()
-    if type(lines[0]) == type(b''):
+    if type(lines[0]) == bytes:
         for i in range(len(lines)):
             lines[i] = lines[i].decode()
+    # Removing first two lines, because they are not XML
     data = ''.join(lines[2:])
     try:
         xml_root = ETree.fromstring(data)
     except ETree.ParseError:
         return None
     runs = []
-    problem, submit_id, user_id, submit_outcome = None, None, None, None
+    submit_id, problem, user_id, submit_outcome = None, None, None, None
     for child in xml_root.iter():
         if child.tag == 'testing-report':
             submit_id = child.attrib['run-id']
@@ -30,6 +31,7 @@ def ejudge_get_submit(file, memory_base, contest_id):
                 memory_base.add_problem(contest_id, problem_id, problem)
         elif child.tag == 'test':
             runs.append(Run(problem, submit_id, child.attrib['num'], child.attrib['status']))
-    if None in (problem, submit_id, user_id, submit_outcome):
+    # If we haven't visit the 'testing-report' tag, we can't say anything about submit
+    if None in (submit_id, problem, user_id, submit_outcome):
         return None
     return Submit(submit_id, problem, user_id, runs, submit_outcome)
