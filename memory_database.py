c28447a542bff4c324172135b69c4ae81202d335
Denis Kalinochkin <kalinochkind@gmail.com>
2015-07-29 12:03:45 +0300

    added EjudgeDB initialization, constructor simplified

--- memory_database.py
+++ memory_database.py
@@ -1,13 +1,18 @@
 import os.path
+from traverse import traverse_contest
+from ejudge_get_submit import ejudge_get_submit
+from ejudgedb import EjudgeDB
 
 
 class MemoryDatabase:
-    def __init__(self, home_dir, traverse_contest, get_submit):
+    def __init__(self, home_dir, csv_filename=None):
         self.submits = []
         self.problems = dict()
         self.home_dir = home_dir.rstrip(os.path.sep)
-        self.traverse_contest = traverse_contest  # function
-        self.get_submit = get_submit  # function
+        if csv_filename is not None:
+            self.database = EjudgeDB(csv_filename)
+        else:
+            self.database = None
         self.init_database()
 
     def init_database(self):
@@ -15,8 +20,8 @@ class MemoryDatabase:
             contest_id = int(os.path.basename(self.home_dir))  # directory name
         except ValueError:
             contest_id = None
-        for file in self.traverse_contest(self.home_dir):
-            self.submits.append(self.get_submit(file, self, contest_id))
+        for file in traverse_contest(self.home_dir):
+            self.submits.append(ejudge_get_submit(file, self, contest_id))
 
     # called from get_submit
     def problem_exists(self, contest_id, problem_id):
