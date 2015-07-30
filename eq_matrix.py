3fe54f47b4d5a3451cf0b5f43afe976eccaa2ce7
Fedor Chukharev <random-s_home@list.ru>
2015-07-30 17:43:03 +0300

    Resolves #52 Resolves #54 fixed get_eq_matrix() and wrote diffrent complex tests

--- eq_matrix.py
+++ eq_matrix.py
@@ -1,24 +1,20 @@
-from submit import Submit
-
 def get_eq_matrix(submits):
-    case_amount = len(submits[0].problem.case_ids) # as submits refer to same problem, case am. should be same
+    case_amount = len(submits[0].problem.case_ids) # as submits refer to same problem, which knows max cases amount
+    
+    matrix = [[0] * case_amount for i in range(case_amount)]
 
-    for i in range(case_amount):
-        matrix.append([0] * case_amount)
-
-    for first in range(case_amount):
-        for second in range(first + 1, case_amount): # warning! Half of the table is left filled with 0!
-            for submit in submits:
-                comp_list = [] # this is needed in order to unite "WA", "ML" etc as fail
-                if submit[first] == "OK":
+    for submit in submits:
+        for first in range(len(submit.runs)):
+            for second in range(len(submit.runs)): 
+                comp_list = [] # this is needed in order to unite "WA", "ML" etc
+                if submit.runs[first].outcome == "OK":
                     comp_list.append(1)
                 else:
                     comp_list.append(0)
-                if submit[second] == "OK":
+                if submit.runs[second].outcome == "OK":
                     comp_list.append(1)
                 else:
                     comp_list.append(0)
-                    
                 if comp_list[0] == comp_list[1]:
                     matrix[first][second] += 1
     return matrix
