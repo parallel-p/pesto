efadb8e81229e51d3e1edf1bc99e2f58fa2e67dc
Ivan Kriuchkov <johnhook059@gmail.com>
2015-07-30 11:29:20 +0300

    Feature #24 removed unnecessaty tests

--- problem_test.py
+++ problem_test.py
@@ -8,14 +8,6 @@ class TestProblemMethods(unittest.TestCase):
         temp = Problem(0, 0, [0, 0])
         self.assertTrue(temp.contest_id == 0 and temp.problem_id == 0 and temp.case_ids == [0, 0])
 
-    def test_init_empty_init(self):
-        with self.assertRaises(TypeError):
-            Problem()
-
-    def test_init_bad_arguments(self):
-        with self.assertRaises(TypeError):
-            Problem([0, 0], sort(), [[0, 0], [0, 0]])
-
     def test_str_white(self):
         temp = Problem('luck', 'duck', [0, 0])
         self.assertEqual(str(temp), 'Contest #luck Problem #duck\nCases: [0, 0]')
