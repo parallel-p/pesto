f90eee0db8e5b0c2f801f40b6c2320fde2412d22
Daniil Konovalenko <ddkono@gmail.com>
2015-07-30 10:04:10 +0300

    added docstring for Problem, removed manual test

--- problem.py
+++ problem.py
@@ -1,8 +1,11 @@
-__author__ = 'Daniil Konovalenko'
-
-
 class Problem:
+
     def __init__(self, contest_id, problem_id, case_ids):
+        """
+        :type contest_id: str or int
+        :type problem_id: str or int
+        :type case_ids: list
+        """
         self.contest_id = contest_id
         self.problem_id = problem_id
         self.case_ids = case_ids
@@ -11,14 +14,3 @@ class Problem:
         return 'Contest #{0} Problem #{1}\nCases: {2}'.format(self.contest_id,
                                                               self.problem_id,
                                                               self.case_ids)
-
-
-# Testing Problem.__str__()
-def main():
-    problem = Problem(input('Contest id: '), input('Problem id: '),
-                      list(map(int, input('Case list: ').split())))
-    print(problem)
-
-
-if __name__ == '__main__':
-    main()
