504388ad1cad186d2eec9c134069752c6762e54b
Andrey Tkachev <tka4.andr@yandex.ru>
2015-07-30 17:04:23 +0300

    Resolv #51 added runs_results field in submit class

--- submit.py
+++ submit.py
@@ -7,6 +7,8 @@ class Submit:
         self.outcome = outcome
         self.user_id = user_id
 
+        self.runs_results = ''.join([str(run.outcome) for run in self.runs])
+
     def __str__(self):
         if len(self.runs) != 0:
             _result = "Submit: {0}; Result: {1}; User id: {2}; Runs: {3}.".format(self.submit_id, self.outcome, self.user_id, (", ").join([str(run) for run in self.runs]))
--- submit_test.py
+++ submit_test.py
@@ -3,10 +3,14 @@ from submit import Submit
 from run import Run
 
 class PositiveTests(unittest.TestCase):
+
     def setUp(self):
         self.runs = []
+        self.runs_results = []
         for i in range(2):
+            self.runs_results.append(str(1))
             self.runs.append(Run(0, 0, i, 1))
+        self.runs_results = ''.join(self.runs_results)
         
     def test_init(self):
         self.submit = Submit(0, 1, 179, self.runs, 1)
@@ -20,9 +24,10 @@ class PositiveTests(unittest.TestCase):
         self.submit = Submit(0, 1, 179, self.runs, 1)
         self.assertEqual(str(self.submit), "Submit: 0; Result: 1; User id: 179; Runs: Case #0 Outcome 1, Case #1 Outcome 1.")
 
-    def test_str_0runs(self):   
-        self.submit = Submit(0, 1, 179, [], 1)
-        self.assertEqual(str(self.submit), "Submit: 0; Result: 1; User id: 179; No Runs.")
+    def test_results_of_runs(self):
+        self.submit = Submit(0, 1, 179, self.runs, 1)
+        self.assertEqual(self.submit.runs_results, self.runs_results)
+
 
 if __name__ == "__main__":
     unittest.main() 
