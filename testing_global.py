9bdd629925e10d904fc987edf8cdb157489c927a
Ivan Kriuchkov <johnhook059@gmail.com>
2015-07-31 10:06:37 +0300

    Support #59

--- testing_global.py
+++ testing_global.py
@@ -8,13 +8,13 @@ base = unittest.TestResult()
 tests.run(base)
 print('RUN - {0}\nERRORS - {1}\nFAILURES - {2}\n'.format(base.testsRun, len(base.errors), len(base.failures)))
 
-if len(base.failures):
+if base.failures:
     print('\nFAILURES:\n')
     for fail in base.failures:
         for s in fail:
             print(s)
 
-if len(base.errors):
+if base.errors:
     print('\nERRORS:\n')
     for error in base.errors:
         for s in error:
