67c50da68010675cf33e1d36da5457c54461944a
Artom-Kozincev <s17b2_kozincev@179.ru>
2015-07-31 12:29:17 +0300

    Ported count_cases (yet without tests) to new architecture. Refs #87.

--- stats/count_cases.py
+++ stats/count_cases.py
@@ -1,26 +1,18 @@
+from visitor import Visitor
 
 
-def count_cases(problems, contest_id): # dict of problems goes here with key
-    _result = []
-    for problem_pair in problems.items():    # problem_pair[0] - key of dict item
-        if problem_pair[0][0] == contest_id: # problem_pair[0][0] - contest_id in key
-            problem = problem_pair[1]        # problem_pair[1] - value of dict item
-            _result.append((problem.contest_id, problem.problem_id, len(problem.case_ids)))
-    _result.sort()
-    return _result
+class CasesCounter(Visitor):
+    def __init__(self):
+        super().__init__()
+        self.result = {}
+    
+    def update_submit(self, submit):
+        if str(submit.problem_id) not in self.result or len(submit.case_ids) > self.result[str(submit.problem_id)]:
+            self.result[str(submit.problem_id)] = len(submit.case_ids)
+    
+    def get_stat_data(self):
+        for k, v in sorted(self.result.items()):
+            result += 'Problem #{}: {} cases.\n'.format(k, v)        
+        return result
 
-def main():
-    print('Enter contests base dir name:')
-    base_dir = input()
-    print('Enter database filename:')
-    database_filename = input()
-    print('Enter contest ID')
-    contest_id = input()
-    database = MemoryDatabase(base_dir, database_filename)
-    return (database.problems, contest_id)
-
-
-if __name__ == "__main__":
-    problems, contest_id = main()
-    for result in count_cases(problems, contest_id):
-        print("Contest ID #{0}, Problem ID #{1}\tCases amount: {2}".format(*result))
+classname = 'CasesCounter'
