b8a38dacae559882993d8b3487cd3eab745623b2
Andrey Tkachev <tka4.andr@yandex.ru>
2015-07-31 09:42:26 +0300

    Refs #66 #67 #68 in submit.py removed _ in local variable, __str__ function refactoring

--- submit.py
+++ submit.py
@@ -1,7 +1,6 @@
 class Submit:
 
     def __init__(self, submit_id, problem, user_id, runs, outcome):
-
         self.problem = problem
         self.submit_id = submit_id
         self.runs = runs
@@ -10,8 +9,4 @@ class Submit:
         self.runs_results = ''.join([str(run.outcome) for run in self.runs])
 
     def __str__(self):
-        if len(self.runs) != 0:
-            _result = "Submit: {0}; Result: {1}; User id: {2}; Runs: {3}.".format(self.submit_id, self.outcome, self.user_id, (", ").join([str(run) for run in self.runs]))
-        else:
-            _result = "Submit: {0}; Result: {1}; User id: {2}; No Runs.".format(self.submit_id, self.outcome, self.user_id)
-        return _result
+        return "Submit: {0}; Result: {1}; User id: {2}; Runs: {3}.".format(self.submit_id, self.outcome, self.user_id, (", ").join([str(run) for run in self.runs]))
