1caf06aa34f77ce3aec9b503508ed6f198615398
Fedor Chukharev <random-s_home@list.ru>
2015-07-31 10:59:38 +0300

    Resolve #76 Defined get_stat_data and updated to Visitor

--- compositor_visitor.py
+++ compositor_visitor.py
@@ -3,10 +3,13 @@ from visitor import Visitor
 class CompositorVisitor(Visitor):
 
     def __init__(self, *visitors):
+        super().__init__(self)
         self.visitors = visitors
 
-    def update(self):
+    def update_submit(self, update_submit):
         for visitor in self.visitors:
-            visitor.add_submit(self.current_submit)
-            visitor.Update()    
+            visitor.update_submit(self.current_submit)
+
+    def get_stat_data(self):
+        "\n\n".join([visitor.get_stat_data() for visitor in self.visitors])
         
