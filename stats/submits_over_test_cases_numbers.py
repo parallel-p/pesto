fd098f062af2ed54a9e5cfae567821af4b508fe1
Daniil Konovalenko <ddkono@gmail.com>
2015-07-31 16:04:59 +0300

    Resolves #95 : Added pretty print for SubmitsOverTestCasesNumbers

--- stats/submits_over_test_cases_numbers.py
+++ stats/submits_over_test_cases_numbers.py
@@ -1,4 +1,5 @@
 from visitor import Visitor
+import operator
 
 
 class SubmitsOverTestCasesNumbers(Visitor):
@@ -18,6 +19,24 @@ class SubmitsOverTestCasesNumbers(Visitor):
             self.number_of_submits_by_number_of_runs_by_problem_id[submit.problem_id] = {}
 
     def pretty_print(self):
-        return str(self.number_of_submits_by_number_of_runs_by_problem_id)
+        data_dict = self.number_of_submits_by_number_of_runs_by_problem_id
+        data_list = sorted(data_dict.items())
+        result = ''
+        for problem in data_list:
+            problem_id = problem[0]
+            test_cases = list(problem[1].keys())
+            submit_numbers = list(problem[1].values())
+            line_multiplier = 100 / max(submit_numbers)
+            result += ('-------------\n'
+                       'Problem #{0}\n'
+                       '-------------\n'.format(problem_id))
+            for i in range(len(test_cases)):
+                result += '{0:{width}} {1} {2}\n'.format(test_cases[i],
+                                                         '#' * int(submit_numbers[i]
+                                                                   * line_multiplier),
+                                                         submit_numbers[i], width=5)
+
+        return result
+
 
 classname = "SubmitsOverTestCasesNumbers"
