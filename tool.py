986f8703fe84c77952c628992ec05e24e057b281
Denis Kalinochkin <kalinochkind@gmail.com>
2015-08-01 16:54:58 +0300

    Resolves #146

--- tool.py
+++ tool.py
@@ -18,29 +18,55 @@ def suppose_csv():
 def suppose_statistics():
     stat_names = [filename[:-3] for filename in os.listdir('stats') if filename.endswith('.py')
                   and not filename.endswith('_test.py') and not filename == '__init__.py']
-    if stat_names:
-        return 'Supposed names:\n---\n{}\n---\n'.format('\n'.join(stat_names))
-    return ''
+    if not stat_names:
+        return ''
+    stat_formatted = ['{}: {}'.format(num, stat) for num, stat in enumerate(stat_names)]
+    return ('Supposed names:\n---\n{}\n---\n'.format('\n'.join(stat_formatted)), stat_names)
 
 
-def get_arguments_from_keyboard():
-    base_dir = input('Enter contests base dir name: ').strip()
+def get_arguments():
+    parser = argparse.ArgumentParser(description="Calculate some statistics")
+    parser.add_argument('-m', '--multicontest', help='base_dir contains several contests', action='store_true')
+    parser.add_argument('-o', '--outfile', help='output file')
+    parser.add_argument('base_dir', nargs='?', default=None, help="directory containing xml's")
+    parser.add_argument('csv_filename', nargs='?', default=None, help="csv file")
+    parser.add_argument('stats_names', default=None, help="names of statistics modules", nargs='*')
+    args = vars(parser.parse_args())
+
+    base_dir = args['base_dir']
+    is_multicontest = args['multicontest']
+    request_multicontest = 0
+
+    if base_dir is None:
+        base_dir = input('Enter contests base dir name: ').strip()
+        request_multicontest = 1
     while not os.path.isdir(base_dir):
         base_dir = input('Please, enter correct directory name: ').strip()
     base_dir = base_dir.rstrip('/').rstrip('\\')
+    if request_multicontest:
+        is_multicontest = input('Does this directory contain only one contest? (y/n) ').lower()
+        while is_multicontest[0] not in 'yn':
+            is_multicontest = input('Enter "y" or "n": ').lower()
+        is_multicontest = is_multicontest[0] == 'n'
 
-    is_multicontest = input('Does this directory contain only one contest? (y/n) ').lower()
-    while is_multicontest[0] not in 'yn':
-        is_multicontest = input('Enter "y" or "n": ').lower()
-    is_multicontest = is_multicontest[0] == 'n'
-
-    csv_filename = input('Enter csv database filename: ' + suppose_csv()).strip()
+    csv_filename = args['csv_filename']
+    if csv_filename is None:
+        csv_filename = input('Enter csv database filename: ' + suppose_csv()).strip()
     while not (os.path.isfile(csv_filename) and os.access(csv_filename, os.R_OK)):
         csv_filename = input('Please, enter correct filename: ').strip()
 
+    stats_names = args['stats_names']
     stats_counters = []
     while not stats_counters:
-        stats_names = input('Enter statistics names (separate by spaces). ' + suppose_statistics()).split()
+        if stats_names is None:
+            supposed_stats = suppose_statistics()
+            stats_names = input('Enter statistics names or numbers (separate by spaces): ' + supposed_stats[0]).split()
+            for i in range(len(stats_names)):
+                if stats_names[i].isdigit():
+                    try:
+                        stats_names[i] = supposed_stats[1][int(stats_names[i])]
+                    except IndexError:
+                        print('There is no statistics', i)
         stats_modules = []
         for stats_name in stats_names:
             if find_spec('stats.' + stats_name) is not None:
@@ -54,46 +80,17 @@ def get_arguments_from_keyboard():
                 print(i.__name__.split('.')[1], 'is broken, skipping')  # TODO something more noticeable
         if not stats_counters:
             print('No statistics selected')  # TODO something more noticeable
+        stats_names = None
 
-    return base_dir, is_multicontest, csv_filename, stats_counters, {}
-
-
-def get_arguments_from_cmdline():
-    parser = argparse.ArgumentParser(description="Calculate some statistics")
-    parser.add_argument('-m', '--multicontest', help='base_dir contains several contests', action='store_true')
-    parser.add_argument('-o', '--outfile', help='output file')
-    parser.add_argument('base_dir', help="directory containing xml's")
-    parser.add_argument('csv_filename', help="csv file")
-    parser.add_argument('stats_names', help="names of statistics modules", nargs='+')
-    args = vars(parser.parse_args())
-    stats_names = args['stats_names']
-    stats_modules = []
-    for stats_name in stats_names:
-        if find_spec('stats.' + stats_name) is not None:
-            stats_modules.append(import_module('stats.' + stats_name))
-        else:
-            print(stats_name, 'not found')
-    stats_counters = []
-    for i in stats_modules:
-        try:
-            stats_counters.append(getattr(i, i.classname)())
-        except AttributeError:
-            print(i.__name__.split('.')[1], 'is broken, skipping')
-    if not stats_counters:
-        print('No statistics selected')
-        exit()
     optional = {}
     if args['outfile']:
         optional['outfile'] = args['outfile']
-    return args['base_dir'], args['multicontest'], args['csv_filename'], stats_counters, optional
+
+    return base_dir, is_multicontest, csv_filename, stats_counters, optional
 
 
 def main():
-    if len(sys.argv) < 2:
-        base_dir, is_multicontest, csv_filename, stats_counters, optional = get_arguments_from_keyboard()
-    else:
-        base_dir, is_multicontest, csv_filename, stats_counters, optional = get_arguments_from_cmdline()
-    base_dir = base_dir.rstrip('/').rstrip('\\')
+    base_dir, is_multicontest, csv_filename, stats_counters, optional = get_arguments()
     if is_multicontest:
         home_dirs = [base_dir + os.path.sep + i for i in os.listdir(base_dir)]
     else:
