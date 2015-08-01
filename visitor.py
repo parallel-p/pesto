8d67bdf57a7446931f2b9aa8db3e666a7d130edd
Daniil Konovalenko <ddkono@gmail.com>
2015-08-01 09:59:09 +0300

    Refs #113 visitor.py update_submit() is now visit()

--- visitor.py
+++ visitor.py
@@ -3,7 +3,7 @@ class Visitor:
         self.result = None
         self.the_number_of_transmitted_submits = 0
 
-    def update_submit(self, submit):
+    def visit(self, submit):
         self.the_number_of_transmitted_submits += 1
 
     # Returns ready for print string of result data
