543df4e8b7f6e9e6dbef302cfcaa04074fff690d
Andrey Tkachev <tka4.andr@yandex.ru>
2015-08-01 11:45:09 +0300

    Refs #107

---
+++ stats/submits_ids_by_signature_visitor.py
@@ -0,0 +1,21 @@
+from visitor import Visitor
+
+
+class SubmitsIdsBySignatueVisitor(Visitor):
+    def __init__(self):
+        self.result = dict()
+
+    def visit(self, submit):
+        if submit.runs_results not in self.result:
+            self.result[submit.runs_results] = [submit.problem_id]
+        else:
+            self.result[submit.runs_results].apend(submit.problem_id)
+
+    def get_stat_data(self):
+        return self.result
+
+    def pretty_print(self):
+        result = []
+        for singnature in self.result:
+            result.append("Signatue:", str(singnature), "Submits ids:",  ''.join(self.result))
+
