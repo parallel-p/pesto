bd2442d8eb91e5e2293df577f6e73a7978048991
Andrey Tkachev <tka4.andr@yandex.ru>
2015-08-02 10:49:10 +0300

    Refs #129 removed filename param from __init__

--- stats/max_test_cases_count.py
+++ stats/max_test_cases_count.py
@@ -3,8 +3,7 @@ import csv
 
 
 class MaxTestCasesCount(Visitor):
-    def __init__(self, file_name):
-        self.file_name = file_name
+    def __init__(self):
         self.max_test_cases_number_by_problem_id = dict()
 
     def visit(self, submit):
@@ -14,10 +13,6 @@ class MaxTestCasesCount(Visitor):
             self.max_test_cases_number_by_problem_id[submit.problem_id] = len(submit.runs)
 
     def get_stat_data(self):
-        csv_file = open(self.file_name, "w")
-        for problem_id in self.max_test_cases_number_by_problem_id:
-            print("\"{0}\";\"{1}\";\"{2}\"".format(problem_id[0], problem_id[1], self.max_test_cases_number_by_problem_id[problem_id]), file=csv_file, sep=";")
-
         return self.max_test_cases_number_by_problem_id
 
 classname = "MaxTestCasesCount"
