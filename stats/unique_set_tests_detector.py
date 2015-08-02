79e1b3e8d66954c7b154b5c57e5d5da0263c8375
Andrey Tkachev <tka4.andr@yandex.ru>
2015-08-02 13:00:57 +0300

    Refs #84 removed long var names

--- stats/unique_set_tests_detector.py
+++ stats/unique_set_tests_detector.py
@@ -1,36 +1,39 @@
 from visitor import Visitor
-import operator
 
 
 class UniqueSetTestsDetector(Visitor):
     def __init__(self):
-        self.submits_number_by_uniqum_runs_results_by_contest_problem_id = dict()
+        #submits_number by uniqum_runs_results by contest_problem_id
+        self.result = dict()
 
     def visit(self, submit):
-        id = '_'.join(submit.problem_id)
-        if id in self.submits_number_by_uniqum_runs_results_by_contest_problem_id:
-            if submit.runs_results in self.submits_number_by_uniqum_runs_results_by_contest_problem_id[id]:
-                self.submits_number_by_uniqum_runs_results_by_contest_problem_id[id][submit.runs_results][1] += 1
+        id = submit.problem_id
+        if id in self.result:
+            if submit.runs_results in self.result[id]:
+                self.result[id][submit.runs_results][1] += 1
             else:
-                self.submits_number_by_uniqum_runs_results_by_contest_problem_id[id][submit.runs_results] = [[run.outcome for run in submit.runs], 1]
+                self.result[id][submit.runs_results] = [[run.outcome for run in submit.runs], 1]
         else:
-            self.submits_number_by_uniqum_runs_results_by_contest_problem_id[id] = {submit.runs_results:[[run.outcome for run in submit.runs], 1]}
+            self.result[id] = {submit.runs_results:[[run.outcome for run in submit.runs], 1]}
 
     def get_sort_result(self):
-        return sorted(self.submits_number_by_uniqum_runs_results_by_contest_problem_id.items())
+        return sorted(self.result.items())
 
     def get_stat_data(self):
-        return self.submits_number_by_uniqum_runs_results_by_contest_problem_id
+        return self.result
 
     def pretty_print(self):
         sorted_result = self.get_sort_result()
         prettty_result = []
+
         for problem_id_and_runs in sorted_result:
             prettty_result.append("***")
-            prettty_result.append("contest_problem #" + problem_id_and_runs[0])
+            prettty_result.append("contest_problem #" + '_'.join(problem_id_and_runs[0]))
+
             for uniqum_result in sorted(problem_id_and_runs[1]):
-                runs_res_in_string = ' '.join(self.submits_number_by_uniqum_runs_results_by_contest_problem_id[problem_id_and_runs[0]][uniqum_result][0])
-                submits_number = self.submits_number_by_uniqum_runs_results_by_contest_problem_id[problem_id_and_runs[0]][uniqum_result][1]
+                runs_res_in_string = ' '.join(self.result[problem_id_and_runs[0]][uniqum_result][0])
+                submits_number = self.result[problem_id_and_runs[0]][uniqum_result][1]
+
                 prettty_result.append("  Run result:{0}\n   Submits count:{1}\n".format(runs_res_in_string, submits_number))
         return '\n'.join(prettty_result)
 
--- stats/unique_set_tests_detector_test.py
+++ stats/unique_set_tests_detector_test.py
@@ -10,10 +10,10 @@ class UniqueSetTestsTest(unittest.TestCase):
     def test_common(self):
         ejudge_parse(['testdata/unique_set_test/1'], 'testdata/unique_set_test/common.csv', self.detector)
         res = self.detector.get_stat_data()
-        self.assertEqual(res['1_1']['OKOKOKOKOKOKOKOKOKOK'][1], 1)
-        self.assertEqual(res['1_1']['OKOKOKOKOK'][1], 2)
-        self.assertEqual(res['1_1']['OKOKOKOKWA'][1], 1)
-        self.assertEqual(res['1_2']['OKOKOKOKWA'][1], 2)
+        self.assertEqual(res[('1', '1')]['OKOKOKOKOKOKOKOKOKOK'][1], 1)
+        self.assertEqual(res[('1', '1')]['OKOKOKOKOK'][1], 2)
+        self.assertEqual(res[('1', '1')]['OKOKOKOKWA'][1], 1)
+        self.assertEqual(res[('1', '2')]['OKOKOKOKWA'][1], 2)
 
     def test_empty(self):
         ejudge_parse(['testdata/unique_set_test/2'], 'testdata/unique_set_test/common.csv', self.detector)
