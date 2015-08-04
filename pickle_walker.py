0238aed53aa43c843cc116ff875c326499f45f5b
Ivan Kriuchkov <johnhook059@gmail.com>
2015-08-04 08:52:46 +0300

    added pickle_waler whith low-strong tests

--- pickle_walker.py
+++ pickle_walker.py
@@ -1,2 +1,20 @@
-def pickle_walker(useless_arg): # Stub
-	return []
+from os import walk
+from os.path import join
+import pickle
+
+
+def pickle_walker(start_dir):
+    for root, dirs, files in walk(start_dir):
+        for file in files:
+            if not file.endswith('.pickle'):
+                continue
+
+            file_name = join(root, file)
+            try:
+                with open(file_name, 'rb') as file:
+                    arr = pickle.load(file)
+            except pickle.UnpicklingError:
+                pass
+
+            for submit in arr:
+                yield submit
--- pickle_walker_test.py
+++ pickle_walker_test.py
@@ -1,8 +1,31 @@
 import unittest
+from unittest.mock import Mock
+from pickle_submits import PickleWriter
+from shutil import rmtree
+from os import mkdir, listdir
+from os.path import exists, join
+from model import Submit
+from pickle_walker import pickle_walker
+
 
 
 class Tester(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.pickle_submit = PickleWriter()
+
+    def tearDown(self):
+        if exists(join(".", "pickle")):
+            rmtree(join(".", "pickle"))
+
+
+    def test_mk_many_pickles(self):
+        submit = Submit(0, 0, 179, 0, [], 1)
+        submit.problem_id = ("17", "0")
+        for i in range(3):
+            self.pickle_submit.visit(submit)
+
+        for i in pickle_walker('pickle/17'):
+            self.assertEqual(submit, i)
 
 
 if __name__ == "__main__":
