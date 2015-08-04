ec028132ad2483bcf5c860961e2bfbd6d35f62f7
Dmitry Strokov <strokovghost@gmail.com>
2015-08-04 15:46:10 +0300

    Refs #204 Added same_runs(kirov/acm) to tool_config

--- tool_config.py
+++ tool_config.py
@@ -1,7 +1,7 @@
 from stats.count_submits import SubmitsCounter
 from stats.eq_matrix import EqMatrix
 from stats.max_test_cases_count import MaxTestCasesCount
-from stats.same_runs import SameRunsKirov
+from stats.same_runs import SameRunsKirov, SameRunsACM
 from stats.submits_ids_by_signature_visitor import SubmitsIdsBySignatureVisitor
 from stats.submits_over_test_cases_numbers import SubmitsOverTestCasesNumbers
 from pickle_submits import PickleWriter
@@ -9,16 +9,18 @@ from visitor_factory import VisitorFactory
 from sharding_visitor import ShardingByContestVisitor
 from sharding_visitor import ShardingByProblemVisitor
 from sharding_visitor import ShardingByLangVisitor
+from elector_visitor import ElectorByMaxCasesVisitor
 
 def get_presets_info():
     return """
         1.count_submits - Counts number of submits for each problem.
         2.eq_matrix - Creates matrix for each problem which contains how many times cases were launched together.
         3.count_cases - Counts number of cases for each problem.
-        4.same_runs - Counts for each problem lists of runs that were launched together.
-        5.submits_by_signature - Counts submits with each outcome for each problem for each language.
-        6.submits_by_tests - Counts submits with each number of launched tests for each problem.
-        7.gen_pickles - Generates fast access information for next use.
+        4.same_runs_kirov - (For Kirov scoring) Counts for each problem lists of runs that were launched together.
+        4.same_runs_acm - (For ACM scoring) Counts for each problem lists of runs that were launched together.
+        6.submits_by_signature - Counts submits with each outcome for each problem for each language.
+        7.submits_by_tests - Counts submits with each number of launched tests for each problem.
+        8.gen_pickles - Generates fast access information for next use.
     """
 
 
@@ -29,15 +31,32 @@ def get_visitor_by_preset(preset):
         return ShardingByProblemVisitor(EqMatrixFactory())
     if preset in ['3', 'count_cases']:
         return ShardingByProblemVisitor(MaxTestCasesCountFactory())
-    if preset in ['4', 'same_runs']:
-        return ShardingByProblemVisitor(SameRunsKirovFactory())
-    if preset in ['5', 'submits_by_signature']:
+    if preset in ['4', 'same_runs_kirov']:
+        return ShardingByProblemVisitor(ElectedSameRunsKirovFactory())
+    if preset in ['5', 'same_runs_acm']:
+        return ShardingByProblemVisitor(ElectedSameRunsACMFactory())
+    if preset in ['6', 'submits_by_signature']:
         return ShardingByProblemVisitor(ShardingByLangFactory())
-    if preset in ['6', 'submits_by_tests']:
+    if preset in ['7', 'submits_by_tests']:
         return SubmitsOverTestCasesNumbers()
-    if preset in ['7', 'gen_pickles']:
+    if preset in ['8', 'gen_pickles']:
         return PickleWriter()
     return None
+
+
+class ElectedSameRunsACMFactory(VisitorFactory):
+    def create(self, key):
+        return ElectorByMaxCasesVisitor(SameRunsACMFactory)
+
+
+class SameRunsACMFactory(VisitorFactory):
+    def create(self, key):
+        return SameRunsACM()
+
+
+class ElectedSameRunsKirovFactory(VisitorFactory):
+    def create(self, key):
+        return ElectorByMaxCasesVisitor(SameRunsKirovFactory)
 
 
 class SameRunsKirovFactory(VisitorFactory):
@@ -48,6 +67,7 @@ class SameRunsKirovFactory(VisitorFactory):
 class SubmitsCounterFactory(VisitorFactory):
     def create(self, key):
         return SubmitsCounter()
+
 
 class MaxTestCasesCountFactory(VisitorFactory):
     def create(self, key):
--- tool_config_test.py
+++ tool_config_test.py
@@ -9,8 +9,8 @@ class TestToolConfig(unittest.TestCase):
         self.assertEqual(type(get_presets_info()), str)
 
     def test_presets(self):
-        presets1 = ['1', 'count_submits', '2', 'eq_matrix', '3', 'count_cases', '4', 'same_runs']
-        presets2 = ['5', 'submits_by_signature', '6', 'submits_by_tests', '7', 'gen_pickles']
+        presets1 = ['1', 'count_submits', '2', 'eq_matrix', '3', 'count_cases', '4', 'same_runs_kirov']
+        presets2 = ['5', 'same_runs_acm', '6', 'submits_by_signature', '7', 'submits_by_tests', '8', 'gen_pickles']
         for preset in presets1 + presets2:
             self.assertEqual(issubclass(type(get_visitor_by_preset(preset)), Visitor), True)
         self.assertEqual(get_visitor_by_preset('kek'), None)
