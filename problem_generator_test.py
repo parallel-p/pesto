f42a2720138254243d034d48bde3ce7b3537f526
Mikhail Putilin <mikle98@yandex.ru>
2015-08-05 11:30:29 +0300

    Ref #225 Added problem_generator_test.py

--- problem_generator_test.py
+++ problem_generator_test.py
@@ -1,36 +1,33 @@
 import unittest
 from unittest.mock import Mock
-from find_same_problems import SameProblemsFinder
+from problem_generator import problem_generator
+import ejudge_contest
+import md5_hasher
 
 
-class TestFindSameProblems(unittest.TestCase):
+class TestProblemGenerator(unittest.TestCase):
     def setUp(self):
-        problems = []
-        problems.append(Mock(cases=['1', '2', '3'], problem_id=('17', '1')))
-        problems[-1].name = 'A'  # Mock(name='A') will not work as you expecting!
-        problems.append(Mock(cases=['4', '5', '6'], problem_id=('17', '2')))
-        problems[-1].name = 'B'
-        problems.append(Mock(cases=['1', '2', '3'], problem_id=('17', '3')))
-        problems[-1].name = 'C'
-        problems.append(Mock(cases=['1', '2', '3'], problem_id=('18', '1')))
-        problems[-1].name = 'D'
-        problems.append(Mock(cases=['4', '5', '6'], problem_id=('18', '2')))
-        problems[-1].name = 'E'
-        problems.append(Mock(cases=['7', '8', '9'], problem_id=('18', '3')))
-        problems[-1].name = '1337'
-        self.finder = SameProblemsFinder(problems)
+        ejudge_contest_object = Mock()
+        ejudge_contest_object.get_contest_id = Mock(return_value='42')
+        ejudge_contest_object.get_problems_ids = Mock(return_value=['1', '2'])
+        ejudge_contest_object.get_short_name_by_problem_id = Mock(return_value='a-plus-b')
+        ejudge_contest_object.get_tests_paths_by_problem_id = Mock(return_value=[('a', 'b'), ('c', 'd')])
+        ejudge_contest.EjudgeContest = Mock(return_value=ejudge_contest_object)
+        md5_hasher.get_hash = Mock(return_value='hash')
 
     def test_common(self):
-        result = self.finder.get_stat_data()
-        self.assertEqual(len(result), 2)
-        problems = [p.problem_id for p in result[0]]
-        self.assertEqual(problems, [('17', '1'), ('17', '3'), ('18', '1')])
-        problems = [p.problem_id for p in result[1]]
-        self.assertEqual(problems, [('17', '2'), ('18', '2')])
-    
-    def test_str(self):
-        result = str(self.finder)
-        self.assertEqual(result, 'Problems A, C, D are same.\nProblems B, E are same.\n')
+        contest_dirs = ['000001', '000179']
+        result = [x for x in problem_generator(contest_dirs)]
+        self.assertEqual(len(result), 4)
+        self.assertEqual(result[0].problem_id, ('42', '1'))
+        self.assertEqual(result[1].problem_id, ('42', '2'))
+        self.assertEqual(result[2].problem_id, ('42', '1'))
+        self.assertEqual(result[3].problem_id, ('42', '2'))
+        for problem in result:
+            self.assertEqual(problem.name, 'a-plus-b')
+            self.assertEqual(problem.cases, ['hash', 'hash'])
+        self.assertEqual(md5_hasher.get_hash.call_args_list, [(('a', 'b'),), (('c', 'd'),)] * 4)
+
 
 if __name__ == "__main__":
     unittest.main()
