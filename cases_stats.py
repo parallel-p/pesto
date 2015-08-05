9f40b03487d36ff8ddb3e754c31e7a6e086a4e0b
Denis Kalinochkin <kalinochkind@gmail.com>
2015-08-05 09:29:52 +0300

    Resolves #214 added CasesStats

---
+++ cases_stats.py
@@ -0,0 +1,11 @@
+class CasesStats:
+
+    def __init__(self, problems):
+        self.problems = problems
+        self.result = None
+
+    def get_stat_data(self):
+        return self.result
+
+    def __str__(self):
+        return ""
---
+++ cases_stats_test.py
@@ -0,0 +1,20 @@
+import unittest
+from cases_stats import CasesStats
+
+
+class TestCasesStats(unittest.TestCase):
+    def setUp(self):
+        self.stats = CasesStats([1, 2])
+
+    def test_init(self):
+        self.assertEqual(self.stats.result, None)
+        self.assertEqual(self.stats.problems, [1, 2])
+        self.assertEqual(str(self.stats), '')
+
+    def test_stat_data(self):
+        self.stats.result = 42
+        self.assertEqual(self.stats.get_stat_data(), 42)
+
+
+if __name__ == "__main__":
+    unittest.main()
