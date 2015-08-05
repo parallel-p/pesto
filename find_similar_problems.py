ab623c36daa6923870fd0a5d989d9b8d41ff7361
Mikhail Putilin <mikle98@yandex.ru>
2015-08-05 13:47:29 +0300

    Refs #237 Добавлен подсчёт разницы между задачами

--- find_similar_problems.py
+++ find_similar_problems.py
@@ -1,8 +1,11 @@
-similar_problems_min_ratio = 0.749999
+similar_problems_min_ratio = 0.5
 
 
 class SimilarProblemsFinder:
-    def __init__(self, problem_list):
+    def __init__(self, problems):
+        self.same_tests_count_dict = dict()
+        self.similarity_dict = dict()
+        problem_list = list(problems)
         self.result = []
         for i in range(len(problem_list)):
             problem_1 = problem_list[i]
@@ -12,16 +15,34 @@ class SimilarProblemsFinder:
                 for problem_2_test in problem_2.cases:
                     if problem_2_test in problem_1_tests:
                         same_tests_count += 1
-                if same_tests_count > similar_problems_min_ratio * min(len(problem_1.cases), len(problem_2.cases)):
+                similarity = same_tests_count / max(len(problem_1.cases), len(problem_2.cases))
+                if similarity > similar_problems_min_ratio:
                     self.result.append((problem_1, problem_2))
+                self.same_tests_count_dict[(problem_1, problem_2)] = same_tests_count
+                self.similarity_dict[(problem_1, problem_2)] = similarity
+                self.same_tests_count_dict[(problem_2, problem_1)] = same_tests_count
+                self.similarity_dict[(problem_2, problem_1)] = similarity
 
     def get_stat_data(self):
         return self.result
 
+    def get_same_tests_count(self, problem_1, problem_2):
+        return self.same_tests_count_dict[(problem_1, problem_2)]
+
+    def get_added_tests_count(self, problem_1, problem_2):
+        return len(problem_2.cases) - self.same_tests_count_dict[(problem_1, problem_2)]
+
+    def get_removed_tests_count(self, problem_1, problem_2):
+        return len(problem_1.cases) - self.same_tests_count_dict[(problem_1, problem_2)]
+
+    def get_similarity(self, problem_1, problem_2):
+        return self.similarity_dict[(problem_1, problem_2)]
+
     def __str__(self):
         resulting_string = ''
-        template = 'Problems {p1} from contest #{c1} and problem {p2} from contest #{c2} are similar.\n'
+        template = 'Problems {p1} from contest #{c1} and problem {p2} from contest #{c2} are similar ({similarity}%).\n'
         for pair in self.result:
             resulting_string += template.format(p1=pair[0].name, c1=pair[0].problem_id[0],
-                                                p2=pair[1].name, c2=pair[1].problem_id[0])
+                                                p2=pair[1].name, c2=pair[1].problem_id[0],
+                                                similarity=int(self.get_similarity(pair[0], pair[1])*100))
         return resulting_string
--- find_similar_problems_test.py
+++ find_similar_problems_test.py
@@ -3,29 +3,42 @@ from unittest.mock import Mock
 from find_similar_problems import SimilarProblemsFinder
 
 
-class TestFindSameProblems(unittest.TestCase):
+class TestFindSimilarProblems(unittest.TestCase):
     def setUp(self):
         self.problems = []
         self.problems.append(Mock(cases=['a1', 'a2', 'a3', 'a4', 'a5', 'a6'], problem_id=('42', '1')))
         self.problems[-1].name = '42a'
         self.problems.append(Mock(cases=['b1', 'b2', 'b3', 'b4', 'b5'], problem_id=('42', '2')))
         self.problems[-1].name = '42b'
-        self.problems.append(Mock(cases=['a1', 'a2', 'a3', 'a4', 'a5', 'a7'], problem_id=('43', '1')))
-        self.problems[-1].name = '43a'
         self.problems.append(Mock(cases=['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7'], problem_id=('43', '2')))
+        self.problems[-1].name = '43a'
+        self.problems.append(Mock(cases=['a1', 'a2', 'a3', 'a4', 'a5', 'a7'], problem_id=('43', '1')))
         self.problems[-1].name = '43b'
+        self.problems.append(Mock(cases=['c1', 'c2', 'c3', 'c4'], problem_id=('44', '1')))
+        self.problems[-1].name = '44a'
         self.finder = SimilarProblemsFinder(self.problems)
 
     def test_common(self):
         result = self.finder.get_stat_data()
         self.assertEqual(len(result), 2)
-        self.assertTrue((self.problems[0], self.problems[2]) in result)
-        self.assertTrue((self.problems[1], self.problems[3]) in result)
+        self.assertTrue((self.problems[0], self.problems[3]) in result)
+        self.assertTrue((self.problems[1], self.problems[2]) in result)
+
+    def test_similarity(self):
+        for problem_1 in self.problems:
+            for problem_2 in self.problems:
+                if problem_1 != problem_2:
+                    self.assertEqual(self.finder.get_same_tests_count(problem_1, problem_2),
+                                     len(set(problem_1.cases) & set(problem_2.cases)))
+                    self.assertEqual(self.finder.get_added_tests_count(problem_1, problem_2),
+                                     len(set(problem_2.cases) - set(problem_1.cases)))
+                    self.assertEqual(self.finder.get_removed_tests_count(problem_1, problem_2),
+                                     len(set(problem_1.cases) - set(problem_2.cases)))
 
     def test_str(self):
         result_string = str(self.finder)
-        correct_string = """Problems 42a from contest #42 and problem 43a from contest #43 are similar.
-Problems 42b from contest #42 and problem 43b from contest #43 are similar.
+        correct_string = """Problems 42a from contest #42 and problem 43b from contest #43 are similar (83%).
+Problems 42b from contest #42 and problem 43a from contest #43 are similar (71%).
 """
         self.assertEqual(sorted(result_string.split('\n')), sorted(correct_string.split('\n')))
 
