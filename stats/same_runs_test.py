7795492f2732b9fdf6ebdf0fdc87c0e36918635a
Ivan Kriuchkov <johnhook059@gmail.com>
2015-08-06 09:50:35 +0300

    Feature #223

--- stats/same_runs_test.py
+++ stats/same_runs_test.py
@@ -2,6 +2,7 @@ import unittest
 from model import Submit
 from model import Run
 from stats.same_runs import SameRunsKirov, SameRunsACM
+from unittest.mock import Mock
 
 
 class PositiveTestsKirov(unittest.TestCase):
@@ -25,7 +26,8 @@ class PositiveTestsKirov(unittest.TestCase):
         #           "10 10 10 10\n" +
         #           "10 10 10 10\n")
 
-        self.assertEqual(self.same.pretty_print(), 'Submits - 10\nEquivalent tests: {1 2 3 4}\n')
+        self.assertEqual(self.same.pretty_print(), 'Submits - 10\nEquivalent tests: {1 2 3 4}\n'
+                                                   'we recommend removing: {2 3 4}\nit will save: 3.0sec')
 
     def test_mixed(self):
         runs = []
@@ -46,7 +48,8 @@ class PositiveTestsKirov(unittest.TestCase):
         for submit in submits:
             self.same.visit(submit)
 
-        self.assertEqual(self.same.pretty_print(), 'Submits - 10\nEquivalent tests: {2 3 4}\nUnique tests: {1}\n')
+        self.assertEqual(self.same.pretty_print(), 'Submits - 10\nEquivalent tests: {2 3 4}\nUnique tests: {1}\n'
+                                                   'we recommend removing: {3 4}\nit will save: 2.0sec')
 
     def test_different(self):
         runs = []
@@ -67,7 +70,8 @@ class PositiveTestsKirov(unittest.TestCase):
         #           "10 0 10 0\n" +
         #           "0 10 0 10\n")
 
-        self.assertEqual(self.same.pretty_print(), 'Submits - 10\nEquivalent tests: {1 3} {2 4}\n')
+        self.assertEqual(self.same.pretty_print(), 'Submits - 10\nEquivalent tests: {1 3} {2 4}\nwe recommend '
+                                                   'removing: {3 4}\nit will save: 2.0sec')
 
     def test_difruns(self):
         runs = []
@@ -112,7 +116,8 @@ class TestsACM(unittest.TestCase):
         self.same.visit(Submit(1, (0, 0), 0, 0, runs, 0, 'ACM'))
         self.same.visit(Submit(1, (0, 0), 0, 0, runs1, 0, 'ACM'))
 
-        self.assertEqual(self.same.pretty_print(), 'Submits - 2\nEquivalent tests: {1 2}\nUnique tests: {3}\n')
+        self.assertEqual(self.same.pretty_print(), 'Submits - 2\nEquivalent tests: {1 2}\nUnique tests: {3}\n'
+                                                   'we recommend removing: {2}\nit will save: 0.2sec')
 
     def test_ACM_problem2(self):
         runs = []
@@ -127,7 +132,8 @@ class TestsACM(unittest.TestCase):
         self.same.visit(Submit(1, (0, 0), 0, 0, runs, 0, 'ACM'))
         self.same.visit(Submit(1, (0, 0), 0, 0, runs1, 0, 'ACM'))
 
-        self.assertEqual(self.same.pretty_print(), 'Submits - 2\nEquivalent tests: {2 3}\nUnique tests: {1}\n')
+        self.assertEqual(self.same.pretty_print(), 'Submits - 2\nEquivalent tests: {2 3}\nUnique tests: {1}\n'
+                                                   'we recommend removing: {3}\nit will save: 0.1sec')
 
     def test_ACM_problem_hard(self):
         runs1 = []
@@ -151,7 +157,8 @@ class TestsACM(unittest.TestCase):
         self.same.visit(Submit(1, (0, 0), 0, 0, runs2, 0, 'ACM'))
         self.same.visit(Submit(1, (0, 0), 0, 0, runs3, 0, 'ACM'))
 
-        self.assertEqual(self.same.pretty_print(), 'Submits - 3\nEquivalent tests: {1 3} {4 5}\nUnique tests: {2}\n')
+        self.assertEqual(self.same.pretty_print(), 'Submits - 3\nEquivalent tests: {1 3} {4 5}\nUnique tests: {2}\n'
+                                                   'we recommend removing: {3 5}\nit will save: 0.3sec')
 
     def test_ACM_problem_1(self):
         runs1 = []
@@ -178,6 +185,28 @@ class TestsACM(unittest.TestCase):
 
         self.assertEqual(self.same.pretty_print(), 'Submits - 1\nUnique tests: {1}\n')
 
+    def test_ACM_(self):
+        run1 = Mock()
+        run1.case_id = 1
+        run1.outcome = 'OK'
+        run1.time = 3000
+
+        run2 = Mock()
+        run2.case_id = 2
+        run2.outcome = 'OK'
+        run2.time = 3500
+
+        run3 = Mock()
+        run3.case_id = 3
+        run3.outcome = 'OK'
+        run3.time = 2700
+
+        submit = Mock()
+        submit.runs = [run1, run2, run3]
+
+        self.same.visit(submit)
+        self.assertEqual(self.same.pretty_print(), 'Submits - 1\nEquivalent tests: {1 2 3}\n'
+                                                   'we recommend removing: {1 2}\nit will save: 6.5sec')
 
 if __name__ == "__main__":
     unittest.main()
