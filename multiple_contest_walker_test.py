d61193cee453f6e2767c35a4a90e04ebf429959d
Daniil Konovalenko <ddkono@gmail.com>
2015-08-06 16:34:10 +0300

    MultipleContestWalker: added tests for non-digit and non-6-chars directories. Refs #227

--- multiple_contest_walker_test.py
+++ multiple_contest_walker_test.py
@@ -5,14 +5,24 @@ import os.path
 
 class TestMultiContestWalker(unittest.TestCase):
     def setUp(self):
+        contests_dir = os.path.join('testdata', 'ejudge_contest')
         self.walker = MultipleContestWalker()
+        self.contests = [contest for contest in self.walker.walk(contests_dir)]
+        self.true_contests = [('1', os.path.join('testdata', 'ejudge_contest', '000001')),
+                         ('2', os.path.join('testdata', 'ejudge_contest', '000002'))]
 
     def test_walk(self):
-        contests_dir = os.path.join('testdata', 'ejudge_contest')
-        contests = [contest for contest in self.walker.walk(contests_dir)]
-        true_contests = [('1', os.path.join('testdata', 'ejudge_contest', '000001')),
-                         ('2', os.path.join('testdata', 'ejudge_contest', '000002'))]
-        self.assertEqual(sorted(contests), sorted(true_contests))
+        self.assertEqual(sorted(self.contests), sorted(self.true_contests))
+
+    def test_non_digit_folder(self):
+        self.assertFalse(('definitely_not_a_contest',
+                          os.path.join('testdata',
+                                       'ejudge_contest',
+                                       'definitely_not_a_contest')) in self.contests)
+
+    def test_non_6_chars_folder(self):
+        self.assertFalse(('420', os.path.join('testdata', 'ejudge_contest', '420')) in
+                         self.contests)
 
 
 if __name__ == "__main__":
