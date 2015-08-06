79f94dc4594f28afa7c04d4ec0f581be70e2b941
Ivan Kriuchkov <johnhook059@gmail.com>
2015-08-06 12:18:41 +0300

    added drawer.py

---
+++ drawer.py
@@ -0,0 +1,18 @@
+class Drawer:
+    def __init__(self, img_size):
+        pass
+
+    def draw_line(self, begin, end, thickness, line_color):
+        pass
+
+    def draw_line_strip(self, array):
+        pass
+
+    def draw_rectangle(self, begin, end, thickness, line_color, rectangle_color):
+        pass
+
+    def draw_text(self, begin, end, size, font, text_color):
+        pass
+
+    def save_png(self, file_name):
+        pass
