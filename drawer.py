37a3a7884255a4284af6a9d2c720208765eb3510
Ivan Kriuchkov <johnhook059@gmail.com>
2015-08-08 09:03:27 +0300

    added drawer.py

--- drawer.py
+++ drawer.py
@@ -1,18 +1,23 @@
+from PIL import Image, ImageDraw
+
+
 class Image:
     def __init__(self, img_size, background_color):
-        pass
+        self.image = Image.new("RGBA", img_size, background_color)
+        self.draw = ImageDraw.Draw(self.image)
 
     def draw_line(self, begin, end, line_thickness, line_color):
-        pass
+        self.draw.line(begin + end, line_color, line_thickness)
 
     def draw_line_strip(self, points, line_thickness, line_color):
         pass
 
     def draw_rectangle(self, begin, size, border_thickness, border_color, rectangle_color):
-        pass
+        self.draw.rectangle(begin + (begin[0] + size[0], begin[1] + size[1]), rectangle_color)
 
-    def draw_text(self, begin, size, font, text_color):
-        pass
+    def draw_text(self, text, begin, size, font, text_color):
+        self.draw.text(begin + (begin[0] + size[0], begin[1] + size[1]), text, text_color, font)
 
     def save_png(self, file_name):
-        pass
+        # del self.draw
+        self.image.save(file_name, 'PNG')
