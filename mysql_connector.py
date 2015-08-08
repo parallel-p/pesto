f41b5ea5a050c98870f36778f5e193b6e09c531b
Fedor Chukharev <random-s_home@list.ru>
2015-08-08 11:15:03 +0300

    Refs #267 Added class MySQLConnector, that can return cursor, open and close connections. Tests are present.

---
+++ mysql_connector.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+class MySQLConnector():
+    def __init__(self):
+        self.connector = mysql.connector
+        self.connection = None
+
+    def create_connection(self, config):
+        self.config = config
+        if self.connector.connection_id != None:
+            self.connection.close()
+        self.connection = self.connector.connect(**self.config)
+        # NO ERRORS ARE CAUGHT HERE
+
+    def get_cursor(self):
+        if connection != None:
+            return self.connection.cursor()
+
+    def close(self):
+        if self.connection != None:
+            self.connection.close()
---
+++ mysql_connector_test.py
@@ -0,0 +1,27 @@
+from unittest.mock import Mock
+from pesto_testcase import PestoTestCase
+from mysql_connector import MySQLConnector
+
+class MySQLConnTest(PestoTestCase):
+    def setUp(self):
+        self.connector = MySQLConnector()
+
+    def test_connection(self):
+        self.connector.create_connection = Mock()
+        config = {'user': 'Scott', 'password': 'qwerty', 'host': '127.0.0.1', 'database': 'ejudge'}
+        self.connector.create_connection(**config)
+        self.connector.create_connection.assert_called_once_with(**config)
+
+    def test_get_cursor(self):
+        self.connector.get_cursor = Mock(return_value="Get this!")
+        result = self.connector.get_cursor()
+        self.connector.get_cursor.assert_called_once_with()
+        self.assertEqual(result, "Get this!")
+
+    def test_close(self):
+        self.connector.close = Mock()
+        self.connector.close()
+        self.connector.close.assert_called_once_with()
+
+if __name__ == "main":
+    unittest.main()
