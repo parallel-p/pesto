794e999a75b58823ea71b8f5ca4760d2899f77a2
Denis Kalinochkin <kalinochkind@gmail.com>
2015-08-08 09:52:55 +0300

    Refs #258 saving to BMP cause some libs for PNG are missing

--- drawer.py
+++ drawer.py
@@ -1,23 +1,28 @@
-from PIL import Image, ImageDraw
+import PIL.Image
+import PIL.ImageDraw
+
+def RGB(r, g, b):
+    return 2 ** 16 * b + 2 ** 8 * g + r
 
 
 class Image:
     def __init__(self, img_size, background_color):
-        self.image = Image.new("RGBA", img_size, background_color)
-        self.draw = ImageDraw.Draw(self.image)
+        self.image = PIL.Image.new("RGBA", img_size, background_color)
+        self.draw = PIL.ImageDraw.Draw(self.image)
 
     def draw_line(self, begin, end, line_thickness, line_color):
         self.draw.line(begin + end, line_color, line_thickness)
 
     def draw_line_strip(self, points, line_thickness, line_color):
-        pass
+        for p1, p2 in zip(points, points[1:]):
+            self.draw_line(p1, p2, line_thickness, line_color)
 
     def draw_rectangle(self, begin, size, border_thickness, border_color, rectangle_color):
-        self.draw.rectangle(begin + (begin[0] + size[0], begin[1] + size[1]), rectangle_color)
+        self.draw.rectangle(begin + (begin[0] + size[0], begin[1] + size[1]), rectangle_color, border_color)
 
     def draw_text(self, text, begin, size, font, text_color):
-        self.draw.text(begin + (begin[0] + size[0], begin[1] + size[1]), text, text_color, font)
+        self.draw.text(begin + (begin[0] + size[0], begin[1] + size[1]), text, text_color)
 
     def save_png(self, file_name):
         # del self.draw
-        self.image.save(file_name, 'PNG')
+        self.image.save(file_name, 'BMP')
