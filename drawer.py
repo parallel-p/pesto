c5e0e3d305ceeb2ae590ea671f28663a3a569832
Denis Kalinochkin <kalinochkind@gmail.com>
2015-08-09 09:06:35 +0300

    Resolves #291

--- drawer.py
+++ drawer.py
@@ -1,6 +1,7 @@
 import PIL.Image
 import PIL.ImageDraw
 
+
 def RGB(r, g, b):
     return 2 ** 16 * b + 2 ** 8 * g + r
 
@@ -14,9 +15,10 @@ class Image:
         self.draw.line(begin + end, line_color, line_thickness)
 
     def draw_line_strip(self, points, line_thickness, line_color):
-        for p1, p2 in zip(points, points[1:]):
-            self.draw_line(p1, p2, line_thickness, line_color)
+        for start_point, end_point in zip(points, points[1:]):
+            self.draw_line(start_point, end_point, line_thickness, line_color)
 
+    # begin is a tuple (x0, y0), size is a tuple (size_x, size_y)
     def draw_rectangle(self, begin, size, border_thickness, border_color, rectangle_color):
         self.draw.rectangle(begin + (begin[0] + size[0], begin[1] + size[1]), rectangle_color, border_color)
 
@@ -24,5 +26,4 @@ class Image:
         self.draw.text(begin + (begin[0] + size[0], begin[1] + size[1]), text, text_color)
 
     def save_png(self, file_name):
-        # del self.draw
         self.image.save(file_name, 'BMP')
