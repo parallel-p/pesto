ec3590db3d52833cc15f7179e30128e688a21f92
Dmitry Strokov <strokovghost@gmail.com>
2015-08-11 16:20:42 +0300

    Fixed small mistakes in DAORuns

--- dao_runs.py
+++ dao_runs.py
@@ -2,19 +2,21 @@ from model import Run
 
 
 class DAORuns:
+    columns = 'realtime, time, outcome, case_ref'
+
     def __init__(self, sqlite_connector):
         self.connector = sqlite_connector
 
     @staticmethod
     def load(row):
-        real_time = row['real_time']
+        real_time = row['realtime']
         time = row['time']
         outcome = row['outcome']
         run = Run('', '', '', real_time, time, outcome)
         return run
 
     def deep_load(self, row):
-        real_time, time, outcome, case_ref = row['real_time'], row['time'], row['outcome'], row['case_ref']
+        real_time, time, outcome, case_ref = row['realtime'], row['time'], row['outcome'], row['case_ref']
         cursor = self.connector.get_cursor()
         cursor.execute('SELECT case_id '
                        'FROM cases '
