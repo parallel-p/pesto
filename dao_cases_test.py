2400cd0fb3256556512ee7c49a3850af20d7900f
Fedor Chukharev <random-s_home@list.ru>
2015-08-11 10:27:41 +0300

    Refs #318 Removed one useless line, that would cause too many questions

--- dao_cases_test.py
+++ dao_cases_test.py
@@ -2,7 +2,7 @@ import unittest
 from unittest.mock import Mock
 from dao_cases import DAOCases
 
-class DAOCasesTest(object):
+class DAOCasesTest():
     def test_load(self):
         row = ("1", "1", "1", "440fae04f9e3619de0b8e9319a6a5f4b")
         self.assertEqual(DAOCases.load(row), "440fae04f9e3619de0b8e9319a6a5f4b")
