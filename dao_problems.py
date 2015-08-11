b78859d0ac5c5889c42bcf3e4e97e5a493a10f30
Artom-Kozincev <s17b2_kozincev@179.ru>
2015-08-11 11:23:35 +0300

    Added new functions from updated interface. Refs #327.

--- dao_problems.py
+++ dao_problems.py
@@ -1,8 +1,23 @@
 from model import Problem
+from dao_cases import DAOCases
 
 
 class DAOProblems:
+    def __init__(self, connector):
+        self.connector = connector
+
     @staticmethod
     def load(row):
         result = Problem(row[2], row[3], [])
+        return result
+
+    def deep_load(self, row):
+        result = DAOProblems.load(row)
+        cursor = self.connector.get_cursor()
+        cursor.execute('SELECT * FROM Cases WHERE problem_ref = ?', (row[0],))
+        cases_row = cursor.fetchone()
+        while cases_row:
+            hash = DAOCases.load(cases_row)
+            result.cases.append(hash)
+            cases_row = cursor.fetchone()
         return result
