3842c31330acc650b89a96ffc63be245d53adbeb
Andrey Tkachev <tka4.andr@yandex.ru>
2015-08-11 15:50:00 +0300

    Refs #284 more popular next problem recomennder

--- stats/last_problem.py
+++
@@ -1,6 +0,0 @@
-def last_problem(user_id, db_cursor):
-    db_cursor.execute('SELECT FIRST 1 problems.problem_id, contests.contest_id'
-                      'FROM problems, users, submits'
-                      'WHERE user_id=? '
-                      'ORDER_BY submits.timestamp', user_id)
-    return tuple(db_cursor.fetchone())
