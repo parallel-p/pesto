c1381ac8a7bd7d88b9a09f4f38e5329cb994d32d
Andrey Tkachev <tka4.andr@yandex.ru>
2015-08-11 12:57:03 +0300

    Refs #317 dao for runs

---
+++ dao_runs_test.py
@@ -0,0 +1,33 @@
+import unittest
+from unittest.mock import Mock
+from pesto_testcase import PestoTestCase
+from dao_runs import DAORuns
+from model import Run
+
+
+class RunsDaoTest(PestoTestCase):
+    def setUp(self):
+        self.cursor = Mock()
+        self.row = ('id', 'submit_ref', 'real_time', 'time', 'outcome', 'case_ref')
+
+    def test_load(self):
+        arg = {'real_time':'real_time', 'time':'time', 'outcome':'outcome'}
+        result = DAORuns.load(arg)
+        run = Run('', '', '', 'real_time', 'time', 'outcome')
+        self.assertEqual(result.real_time, run.real_time)
+        self.assertEqual(result.time, run.time)
+        self.assertEqual(result.outcome, run.outcome)
+
+    def test_deep_load(self):
+        conn = Mock()
+        curs = Mock()
+        curs.fetchone = ('case_id')
+        conn.get_cursor = curs
+
+        arg = {'real_time':'real_time', 'time':'time', 'outcome':'outcome', 'case_ref':'case_ref'}
+        result = DAORuns(conn).deep_load(arg)
+
+        run = Run(None, None, 'case_id', 'real_time', 'time', 'outcome')
+        self.assertEqual(result.real_time, run.real_time)
+        self.assertEqual(result.time, run.time)
+        self.assertEqual(result.outcome, run.outcome)
