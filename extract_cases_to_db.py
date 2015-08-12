dd1e6b86755d5bc3b38096b4a95679a036b6006c
Daniil Konovalenko <ddkono@gmail.com>
2015-08-12 08:42:46 +0300

    Added more logs. update and clean parameters are now avaliable for db_tool

--- extract_cases_to_db.py
+++ extract_cases_to_db.py
@@ -9,6 +9,7 @@ def extract_cases_to_db(contest_dirs, cursor, origin):
         return
 
     for problem in problems:
+        print(problem.problem_id)
         contest_response = cursor.execute('SELECT id FROM Contests WHERE origin = ? AND contest_id = ?',
                                     (origin, problem.problem_id[0].rjust(6, '0'))).fetchone()
         if contest_response is None:
@@ -23,6 +24,21 @@ def extract_cases_to_db(contest_dirs, cursor, origin):
             continue
 
         problem_ref = problem_response[0]
+        hashed_cases = 0
+
+        if problem.problem_id in [5, '5']:
+            print(problem.cases)
+            print(problem.problem_id)
         for case_num in range(len(problem.cases)):
+
             cursor.execute('UPDATE Cases SET io_hash = ? WHERE problem_ref = ? AND case_id = ?',
                             (problem.cases[case_num], problem_ref, case_num + 1))
+            if hashed_cases % 10 == 0:
+                print('Filled in {0} cases of problem # {1} from contest #{2}'.format(hashed_cases,
+                                                                                      problem.problem_id[1],
+                                                                                      problem.problem_id[0]))
+            hashed_cases += 1
+
+        print('Filled in {0} cases of problem # {1} from contest #{2}'.format(hashed_cases,
+                                                                              problem.problem_id[1],
+                                                                              problem.problem_id[0]))
--- fill_database.py
+++ fill_database.py
@@ -27,7 +27,14 @@ def fill_from_xml(sqlite_cursor, ejudge_cursor, start_dir, origin):
     for contest_id, contest_dir in MultipleContestWalker().walk(start_dir):
         print("Filling contest #{0}".format(contest_id))
         walker.contest_id = contest_id
+        processed_submits = 0
         for filename in EjudgeRunsFilesWorker().walk(contest_dir):
             for submit in walker.walk(filename[1]):
                 if submit is not None:
                     filler.fill_db_from_submit(submit, origin)
+                    processed_submits += 1
+                    if processed_submits % 100 == 0:
+                        print('Filled in {0} submits from contest #{1}'.format(processed_submits,
+                                                                               contest_id))
+        print('Contest #{0} was finished, filled in {1} submits'.format(contest_id,
+                                                                     processed_submits))
