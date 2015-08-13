fc06d77cdab645fdfe3124431521897acfeb734e
Artom-Kozincev <s17b2_kozincev@179.ru>
2015-08-13 09:48:05 +0300

    Fixed bug with non-existing problem. Resolves #344.

--- fill_db_from_contest_xml.py
+++ fill_db_from_contest_xml.py
@@ -8,4 +8,4 @@ def fill_db_from_contest_xml(contest_xmls_dir, cursor, origin):
     for extension, filename in files_walker.walk(contest_xmls_dir):
         contest_name = ejudge_get_contest_name(filename)
         contest_id = os.path.basename(filename)[:6]
-        cursor.execute('UPDATE Contests SET name = ? WHERE origin = ? AND contest_id = ?', (contest_name, origin, contest_id.ljust(6, '0')))
+        cursor.execute('UPDATE Contests SET name = ? WHERE origin = ? AND contest_id = ?', (contest_name, origin, contest_id.rjust(6, '0')))
