66b5216e918df71f8b5bb26dc874736a52a999a5
Dmitry Strokov <strokovghost@gmail.com>
2015-08-13 08:53:48 +0300

    Refs #335 Added functions and tests for DAOUsers

--- dao_users.py
+++ dao_users.py
@@ -13,4 +13,31 @@ class DAOUsers:
         return result
 
     def deep_load(self, row):
-        return DAOUsers.load(row)
+        return self.load(row)
+
+    def define(self, origin, user_id):
+        ref = self.lookup(origin, user_id)
+        if ref is None:
+            ref = self.create(origin, user_id)
+        return ref
+
+    def lookup(self, origin, user_id):
+        cursor = self.connector.get_cursor()
+        cursor.execute('SELECT id FROM Users WHERE origin = ? AND user_id = ?', [origin, user_id])
+        response = cursor.fetchone()
+        return response['id'] if response else None
+
+    def create(self, origin, user_id):
+        cursor = self.connector.get_cursor()
+        cursor.execute('INSERT INTO Users (id, origin, user_id) VALUES (NULL, ?, ?)', [origin, user_id])
+        return cursor.lastrowid
+
+    def update(self, ref, update_def):
+        cursor = self.connector.get_cursor()
+        cursor.execute('SELECT {} FROM Users WHERE id = ?'.format(self.columns), ref)
+        old = self.load(cursor.fetchone())
+        new_def = {'origin': old.origin, 'user_id': old.user_id}
+        for key, value in update_def.items():
+            new_def[key] = value
+        new_def['id'] = ref
+        cursor.execute('UPDATE Users SET origin = :origin, user_id = :user_id WHERE id = :id', new_def)
---
+++ dao_users_test.py
@@ -0,0 +1,73 @@
+import unittest
+from unittest.mock import Mock, call
+from dao_users import DAOUsers
+import dao_users
+
+
+class DAOUsersTest(unittest.TestCase):
+    def setUp(self):
+        self.row = {'origin': 'origin', 'user_id': 'user_id'}
+        self.cursor = Mock()
+        connection = Mock()
+        connection.get_cursor.return_value = self.cursor
+        self.dao = DAOUsers(connection)
+        dao_users.User = Mock(return_value='User_object')
+
+    def test_load(self):
+        self.assertEqual(self.dao.load(self.row), 'User_object')
+        self.assertEqual(dao_users.User.mock_calls, [call('user_id', 'origin')])
+
+    def test_deep_load(self):
+        self.dao.load = Mock(return_value='Load')
+        self.assertEqual(self.dao.deep_load('row'), 'Load')
+        self.assertEqual(self.dao.load.mock_calls, [call('row')])
+
+    def test_define(self):
+        self.dao.lookup = Mock(side_effect=[None, 2])
+        self.dao.create = Mock(return_value=1)
+        self.assertEqual(self.dao.define('origin1', 'user_id1'), 1)
+        self.assertEqual(self.dao.define('origin2', 'user_id2'), 2)
+        calls = [call('origin1', 'user_id1'), call('origin2', 'user_id2')]
+        self.assertEqual(self.dao.lookup.mock_calls, calls)
+        calls = [call('origin1', 'user_id1')]
+        self.assertEqual(self.dao.create.mock_calls, calls)
+
+    def test_lookup(self):
+        self.cursor.fetchone.side_effect = [{'id': 1}, None]
+        self.assertEqual(self.dao.lookup('origin1', 'user_id1'), 1)
+        self.assertEqual(self.dao.lookup('origin2', 'user_id2'), None)
+        calls = [call.execute('SELECT id FROM Users WHERE origin = ? AND user_id = ?',
+                              ['origin1', 'user_id1']),
+                 call.fetchone(),
+                 call.execute('SELECT id FROM Users WHERE origin = ? AND user_id = ?',
+                              ['origin2', 'user_id2']),
+                 call.fetchone()]
+        self.assertEqual(self.cursor.mock_calls, calls)
+
+    def test_create(self):
+        self.cursor.lastrowid = 1
+        self.assertEqual(self.dao.create('origin', 'user_id'), 1)
+        calls = [call.execute('INSERT INTO Users (id, origin, user_id) VALUES (NULL, ?, ?)',
+                              ['origin', 'user_id'])]
+        self.assertEqual(self.cursor.mock_calls, calls)
+
+    def test_update(self):
+        self.cursor.fetchone.return_value = None
+        user1, user2 = Mock(), Mock()
+        user1.origin, user1.user_id = 'origin1', 'user_id1'
+        user2.origin, user2.user_id = 'origin2', 'user_id2'
+        self.dao.load = Mock(side_effect=[user1, user2])
+        self.dao.update(1, {'origin': 'origin3'})
+        self.dao.update(2, {'user_id': 'user_id3'})
+        calls = [call.execute('SELECT origin, user_id FROM Users WHERE id = ?', 1),
+                 call.fetchone(),
+                 call.execute('UPDATE Users SET origin = :origin, user_id = :user_id '
+                              'WHERE id = :id', {'origin': 'origin3', 'user_id': 'user_id1', 'id': 1}),
+                 call.execute('SELECT origin, user_id FROM Users WHERE id = ?', 2),
+                 call.fetchone(),
+                 call.execute('UPDATE Users SET origin = :origin, user_id = :user_id '
+                              'WHERE id = :id', {'origin': 'origin2', 'user_id': 'user_id3', 'id': 2})]
+        self.assertEqual(self.cursor.mock_calls, calls)
+
+if __name__ == "__main__":
+    unittest.main()
