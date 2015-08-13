6fa999ad8a4f6c5bf71544795bd368d1d82f8bcb
Daniil Konovalenko <ddkono@gmail.com>
2015-08-13 12:56:26 +0300

    Refs #343. Now db_tool.py handles all exceptions and does not collect all contests names before filling in hashes.

--- walker.py
+++ walker.py
@@ -22,13 +22,17 @@ class SingleContestWalker(Walker):
 
 
 class MultipleContestWalker(Walker):
-    def walk(self, start_dir = os.path.dirname(__file__)):
+    def walk(self, start_dir = os.path.dirname(__file__), path_only=False):
         for root, dirs, files in os.walk(start_dir):
             contest_id = os.path.split(root)[1]
             if ('conf' in dirs and ('problems' in dirs or 'tests' in dirs)
                                and contest_id.isdigit()
                                and len(contest_id) == 6):
-                yield (contest_id.lstrip('0'), root)
+                if path_only:
+                    yield root
+                else:
+                    yield (contest_id.lstrip('0'), root)
+
 
 
 class EjudgeRunsFilesWorker(Walker):
