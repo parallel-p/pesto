d1d7448fd34be276579700ce977775296e265c12
Denis Kalinochkin <kalinochkind@gmail.com>
2015-08-17 09:03:06 +0300

    Removed some unnecessary files

--- case_counter_test.py
+++ case_counter_test.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import Mock
-from stats.count_cases import CasesCounter
+from case_counter import CasesCounter
 
 
 class TestCountCases(unittest.TestCase):
@@ -9,9 +9,6 @@ class TestCountCases(unittest.TestCase):
                          Mock(cases=[i for i in range(3)], problem_id=('17', '2')),
                          Mock(cases=[i for i in range(4)], problem_id=('17', '3'))]
         self.counter = CasesCounter(self.problems)
-
-    def test_init(self):
-        self.assertEqual(self.counter.result, {})
 
     def test_get_stat_data(self):
         self.assertEqual(self.counter.get_stat_data(), {('17', '1'): 2,
@@ -28,9 +25,9 @@ class TestCountCases(unittest.TestCase):
 
     def test_str(self):
         self.counter.get_stat_data()
-        self.assertEqual(str(self.counter), 'Contest #17 Problem #1: 2 case(s)\n'
-                                            'Contest #17 Problem #2: 3 case(s)\n'
-                                            'Contest #17 Problem #3: 4 case(s)\n')
+        self.assertEqual(str(self.counter), 'Contest #000017 Problem #1: 2 case(s)\n'
+                                            'Contest #000017 Problem #2: 3 case(s)\n'
+                                            'Contest #000017 Problem #3: 4 case(s)\n')
 
 if __name__ == "__main__":
     unittest.main()
