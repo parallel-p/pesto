d05b548eea807046698005e47f19a1936c91f894
Mikhail Putilin <mikle98@yandex.ru>
2015-08-21 17:44:21 +0600

    Bugfix in problems_tree

--- problems_tree.py
+++ problems_tree.py
@@ -14,17 +14,18 @@ class ProblemsTree:
                 print("Build tree: ", index, "/", len(problems), file=sys.stderr, sep="")
             problem_1_tests = set(problem_1.cases)
             for problem_2 in self.problems[index + 1:]:
-                same_tests_count = len(problem_1_tests & set(problem_2.cases))
+                problem_2_tests = set(problem_2.cases)
+                same_tests_count = len(problem_1_tests & problem_2_tests)
                 try:
-                    similarity = same_tests_count / max(len(problem_1.cases), len(problem_2.cases))
+                    similarity = same_tests_count / max(len(problem_1_tests), len(problem_2_tests))
                 except ZeroDivisionError:
                     similarity = 0.0
                 if similarity > SIMILAR_PROBLEMS_MIN_RATIO:
                     if problem_2 not in self.problem_previous or\
                        similarity > self.problem_previous[problem_2][1] - EPS:
                         self.problem_previous[problem_2] = (problem_1, similarity, same_tests_count,
-                                                            len(problem_2.cases) - same_tests_count,
-                                                            len(problem_1.cases) - same_tests_count)
+                                                            len(problem_2_tests) - same_tests_count,
+                                                            len(problem_1_tests) - same_tests_count)
 
     def get_problems(self):
         return self.problems
