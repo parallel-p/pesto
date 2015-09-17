c0737674c94567e88bc78383e65ba3cf19a5c6f2
Denis Kalinochkin <kalinochkind@gmail.com>
2015-09-17 17:34:10 +0300

    Fixed case_counter output for problems with one case

--- case_counter.py
+++ case_counter.py
@@ -14,7 +14,7 @@ class CasesCounter(CasesStats):
     def __str__(self):
         result = ''
         for problem_id, cases in sorted(self.result.items(), key=lambda x: (int(x[0][0]), int(x[0][1]))):
-            result += 'Contest #{0} Problem #{1}: {2} case(s)\n'.format(problem_id[0].rjust(6, '0'),
+            result += 'Contest #{0} Problem #{1}: {2} case{3}\n'.format(problem_id[0].rjust(6, '0'),
                                                                         problem_id[1],
-                                                                        cases)
+                                                                        cases, '' if cases == 1 else 's')
         return result
--- tests/case_counter_test.py
+++ tests/case_counter_test.py
@@ -6,19 +6,19 @@ from case_counter import CasesCounter
 
 class TestCountCases(unittest.TestCase):
     def setUp(self):
-        self.problems = [Mock(cases=[i for i in range(2)], problem_id=('17', '1')),
+        self.problems = [Mock(cases=[i for i in range(1)], problem_id=('17', '1')),
                          Mock(cases=[i for i in range(3)], problem_id=('17', '2')),
                          Mock(cases=[i for i in range(4)], problem_id=('17', '3'))]
         self.counter = CasesCounter(self.problems)
 
     def test_get_stat_data(self):
-        self.assertEqual(self.counter.get_stat_data(), {('17', '1'): 2,
+        self.assertEqual(self.counter.get_stat_data(), {('17', '1'): 1,
                                                         ('17', '2'): 3,
                                                         ('17', '3'): 4
         }
         )
 
-        self.assertEqual(self.counter.result, {('17', '1'): 2,
+        self.assertEqual(self.counter.result, {('17', '1'): 1,
                                                ('17', '2'): 3,
                                                ('17', '3'): 4
         }
@@ -26,9 +26,9 @@ class TestCountCases(unittest.TestCase):
 
     def test_str(self):
         self.counter.get_stat_data()
-        self.assertEqual(str(self.counter), 'Contest #000017 Problem #1: 2 case(s)\n'
-                                            'Contest #000017 Problem #2: 3 case(s)\n'
-                                            'Contest #000017 Problem #3: 4 case(s)\n')
+        self.assertEqual(str(self.counter), 'Contest #000017 Problem #1: 1 case\n'
+                                            'Contest #000017 Problem #2: 3 cases\n'
+                                            'Contest #000017 Problem #3: 4 cases\n')
 
 
 if __name__ == "__main__":
